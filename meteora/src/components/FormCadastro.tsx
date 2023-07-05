5cbb1394393225a706770af105cd159d9fb1c017
Enyus <enyus@hotmail.com>
2023-07-05 18:37:47 -0300

    incluída validação (simplista) do form de email

--- meteora/src/components/FormCadastro.tsx
+++ meteora/src/components/FormCadastro.tsx
@@ -4,10 +4,19 @@ import { useState } from "react";
 import ModalCadastro from "./ModalCadastro";
 
 export default function FormCadastro () {
-let [modalStatus, setModalStatus] = useState(false)
+    let [modalStatus, setModalStatus] = useState(false);
+
+    const handleSubmitForm = (e: { preventDefault: any; }) => {
+        e.preventDefault();
+        setModalStatus(true);
+    }
+
     return (
         <section className="p-7 sm:p-9 lg:py-16 lg:px-[25%]">
-            <form className="p-6 border border-black">
+            <form 
+                className="p-6 border border-black"
+                onSubmit={handleSubmitForm}
+            >
                 <p className="mb-6 text-center">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</p>
                 <div className="flex md:w-2/3 mx-auto">
                     <input 
@@ -16,11 +25,11 @@ let [modalStatus, setModalStatus] = useState(false)
                         id="email" 
                         placeholder="Digite seu email" 
                         className="border border-black py-2 px-4 w-full"
+                        required
                     />
                     <button
                         className="bg-primary-purple text-white px-4 py-2"
-                        type="button"
-                        onClick={()=>setModalStatus(true)}
+                        type="submit"
                     >
                         Enviar
                     </button>
